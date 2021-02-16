1287aa99f4e134dd279dd4701ea2dea102cb286b
cmabirua <cmabirua@gmail.com>
2021-02-17 02:44:11 +0530

    Update Text Box

--- src/UI/TextBox/TextBox.js
+++ src/UI/TextBox/TextBox.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
+import Styles from "./TextBox.module.css"
 export default function TextBox(props){
+
     return (
             <Grid item xs={props.xs} sm={props.sm}>
-              <TextField
+              <input
                 autoComplete={props.auto}
                 name={props.name}
-                variant="outlined"
                 required
-                fullWidth
+                style={{width:`${props.width}`}}
                 id={props.id}
                 label={props.label}
                 autoFocus={props.autofocus}
+                className={Styles.TextBox}
+                placeholder={props.placeholder}
               />
         </Grid>
         
