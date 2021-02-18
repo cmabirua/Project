9b01c0f5812b867b66b3dba56f4478968dafb6ae
cmabirua <cmabirua@gmail.com>
2021-02-18 10:03:11 +0530

    Change Route

--- src/Component/Layout/Layout.js
+++ src/Component/Layout/Layout.js
@@ -1,19 +1,19 @@
 import React from "react";
-import {BrowserRouter,Route} from "react-router-dom"
-import Login from "../Login/Login"
+import { BrowserRouter, Route } from "react-router-dom";
+import Login from "../Login/Login";
+import Main from "../Main/Main";
 import SignUp from "../SignUp/SignUp";
 import Topic from "../Topic/Topic";
+import TopicContent from "../TopicContent/TopicContent";
 
-export default function Layout(){
-    return (
-
+export default function Layout() {
+  return (
     <BrowserRouter>
-        {/* <Route render={() =><h1>Hello</h1>} /> */}
-        
-        <Route path="/login" exact component={Login}></Route>
-        <Route path="/signup" exact component={SignUp}></Route>
-        <Route path="/Topic" exact component={Topic}></Route>
-        <Route path="/" exact render={() =><h1>Hello</h1>} />
+      <Route path="/login" exact component={Login}></Route>
+      <Route path="/signup" exact component={SignUp}></Route>
+      <Route path="/Topic" exact component={Topic}></Route>
+      <Route path="/" exact component={Main} />
+      <Route path="/:id" exact component={TopicContent} />
     </BrowserRouter>
-    )
+  );
 }
