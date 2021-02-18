1563d2be4475013d23ae7a0cbeffb6f96c52cbef
cmabirua <cmabirua@gmail.com>
2021-02-18 10:05:43 +0530

    Create Topic Content Page Topic Details

---
+++ src/Component/TopicContent/TopicContent.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Styles from "./TopicContent.module.css"
+export default function TopicContent(props){
+    console.log(props.match.params.id);
+    return(
+        <div></div>
+    )
+}
